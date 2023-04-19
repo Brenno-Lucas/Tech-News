beac782e4aaed75df161c5bea1ebccd8fa28eeb9
Brenno-Lucas <brennolucas8@gmail.com>
2023-04-19 14:59:48 -0300

    Requisitos 1-3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,16 +1,36 @@
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(
+            url, timeout=3, headers={"user-agent": "Fake user-agent"}
+        )
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    elements = selector.css(".entry-title")
+    list = []
+    for element in elements:
+        list.append(element.css("a").xpath("@href").get())
+    return list
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page = selector.css("a.next::attr(href)").get()
+    return next_page
 
 
 # Requisito 4
