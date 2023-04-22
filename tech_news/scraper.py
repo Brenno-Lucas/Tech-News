e64764c37e18e302dae30a9b833832b4ea674709
Brenno-Lucas <brennolucas8@gmail.com>
2023-04-22 10:26:56 -0300

    Requisito 04-05

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
-import time
 from parsel import Selector
+from tech_news.database import create_news
+import time
 
 
 # Requisito 1
@@ -35,9 +36,41 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news = {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css(".entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".author a::text").get(),
+        "reading_time": int(
+            selector.css("li.meta-reading-time::text").get().split()[0]
+        ),
+        "summary": selector.css(".entry-content p")
+        .xpath("string()")
+        .get()
+        .strip(),
+        "category": selector.css(".category-style .label::text").get(),
+    }
+    return news
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = 'https://blog.betrybe.com'
+    url_list = []
+    news = []
+    content = fetch(url)
+    url_list.extend(scrape_updates(content))
+
+    while len(url_list) < amount:
+        next_page = scrape_next_page_link(content)
+        content = fetch(next_page)
+        url_list.extend(scrape_updates(content))
+
+    for url in url_list[:amount]:
+        new_url = fetch(url)
+        fetch_news = scrape_news(new_url)
+        news.append(fetch_news)
+
+    create_news(news)
+    return news
